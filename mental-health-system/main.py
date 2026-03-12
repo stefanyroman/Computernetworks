9e7b913a2afa18704c627111eeda1092f5215af8
stefanyroman <stefanyroman@yahoo.com>
2026-03-12 19:38:56 +0000

    added intro

--- mental-health-system/main.py
+++ mental-health-system/main.py
@@ -2,10 +2,12 @@ from client import send_checkin
 
 
 def main():
-    Stressed = int(input("Stress level (1-10):"))
-    Happy = int(input("Content Level (1-10)"))
-    Motivation = int(input("Motivated level (1-10)"))
+    print("Hello:) This is your Mental Health Check-In")
 
-    send_checkin(Stressed, Happy, Motivation)
+    stressed = int(input("Stress level (1-10):"))
+    happy = int(input("Content Level (1-10)"))
+    motivation = int(input("Motivated level (1-10)"))
+
+    send_checkin(stressed, happy, motivation)
 if __name__ == "__main__":
     main()
