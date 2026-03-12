8da8d6853dea11317053ca5ba3a560d3f838f0fb
stefanyroman <stefanyroman@yahoo.com>
2026-03-12 19:59:45 +0000

    some

---
+++ mental-health-system/server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from algorithm import calculate_mood
+
+app = Flask(__name__)
+
+@app.route("/checkin", methods=["POST"])
+def checkin():
+
+    data = request.json
+
+    mood = calculate_mood(
+        data["stressed"],
+        data["happy"],
+        data["motivation"]
+    )
+
+    return jsonify({"mood:mood"})
+
+    app.run()
