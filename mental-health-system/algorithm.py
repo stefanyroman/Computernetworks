079ffa19adac3ebf8437593cdfd444748217420c
stefanyroman <stefanyroman@yahoo.com>
2026-03-12 19:43:24 +0000

    algorithm

---
+++ mental-health-system/algorithm.py
@@ -0,0 +1,9 @@
+def calculate_mood(stressed, happy, motivation):
+    score = (happy + motivation) - stressed
+
+    if score > 5:
+        return "Positive"
+    elif score >= 0"
+        return "Neutral"
+    else:
+        return "Needs Help - Check in :( "
