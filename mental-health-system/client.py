274ae4a8f00f4646add188a35ae1e32fe7533485
brooke006 <143116456+brooke006@users.noreply.github.com>
2026-04-18 20:28:24 -0500

    client

--- mental-health-system/client.py
+++ mental-health-system/client.py
@@ -1,6 +1,9 @@
+#allows python to send requst to server
 import requests
+# sends the results to the flask server
 def send_checkin(stressed, happy, motivation, test_id):
 
+    #put the data into a labled format
     data = {
         "test_id": test_id,
         "stressed": stressed,
@@ -9,12 +12,16 @@ def send_checkin(stressed, happy, motivation, test_id):
     }
 
     try:
+        #send the data to the server
         response = requests.post("http://127.0.0.1:5000/checkin", json=data)
 
+        #if request works print result
         if response.status_code == 200:
             print(response.json().get("mood"))
         else:
+            #if error show error code
             print(f"Error: {response.status_code}")
 
     except requests.exceptions.ConnectionError:
+        #this error happens when the client cannot connect to the server, likely because server.py is not running
         print("Could not connect to the server. Please ensure that server.py is running.")
